edf4ad7bb10125432c90a08502c91f267b9d8dfb
Удалов В.С <udalov@gbu.volgo-balt.ru>
2022-10-08 13:26:02 +0300

    first try. seems working

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# praktikum_new_diplom
+### What is about
+
+Project foodgram allow you to watch, create recipes
+subscribe on users (except yourself)
+and download shopping list of ingedients.
+
+### How to start project:
+
+Clone repo and go to directory in command line:
+
+```
+git clone https://github.com/Vootau/foodgram-project-react.git
+```
+
+```
+cd backend
+```
+
+Create and activate virtual environment:
+
+```
+python -m venv venv
+```
+
+```
+source venv/Scripts/activate
+```
+
+```
+python -m pip install --upgrade pip
+```
+
+Install dependences from requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+
+Make migrations:
+
+```
+python manage.py migrate
+```
+
+Upload ingerdients from .csv file (if nesessary):
+
+```
+python manage.py import csv
+```
+
+Run project:
+
+```
+python manage.py runserver
+
+python manage.py createsuperuser
+```
+
---
+++ backend/api/views/users_views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import Follow, User
+from api.pagination import LimitPageNumberPagination
+from api.serializers.users_serializers import SubscriptionsSerializer
+
+
+class ListSubscriptions(viewsets.ModelViewSet):
+    serializer_class = SubscriptionsSerializer
+    pagination_class = LimitPageNumberPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = User.objects.filter(following__user=user)
+        return queryset
+
+
+class Subscribe(APIView):
+    def delete(self, request, pk):
+        user = request.user
+        author = get_object_or_404(User, id=pk)
+        if not (
+            user == author or Follow.objects.filter(user=user,
+                                                    author=author).exists()
+        ):
+            return Response(
+                {"errors": "Subscription not available"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        subscription = get_object_or_404(Follow, user=user, author=author)
+        subscription.delete()
+        return Response(
+            {"errors": "Successful resubscription"}, status=status.HTTP_204_NO_CONTENT
+        )
+
+    def post(self, request, pk):
+        user = request.user
+        author = get_object_or_404(User, id=pk)
+        if user == author or Follow.objects.filter(user=user,
+                                                   author=author).exists():
+            return Response(
+                {
+                    "errors": "Subscription not available "
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        Follow.objects.get_or_create(user=user, author=author)
+        serializer = SubscriptionsSerializer(author,
+                                             context={"request": request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
